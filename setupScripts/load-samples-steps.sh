5d8f5e360be706f624a818e0253029cc4c76d9e1
Lynn Langit <lynnlangit@live.com>
2016-10-04 22:20:30 -0700

    added load samples script

---
+++ setupScripts/load-samples-steps.sh
@@ -0,0 +1,13 @@
+# SSH to GCE / Galaxy instance
+
+# change to galaxy user
+sudo su ubuntu 
+
+# change to galaxy directory
+cd /opt/galaxy/tools/
+
+# clone sample tools
+git clone https://github.com/Sage-Bionetworks/SMC-het-challenge-examples.git
+
+# restart galaxy to load new tools
+restart_galaxy
