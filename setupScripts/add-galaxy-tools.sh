65ef6da62257f61f28602ef3eaaeaebf5c78c0ee
Lynn Langit <lynnlangit@live.com>
2016-10-08 21:39:55 -0700

    added script to load galaxy tools

---
+++ setupScripts/add-galaxy-tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ---------------Load example Galaxy Tools------------
+# Using the GCP (GCE) console, click the 'SSH' button to open an SSH shell
+
+sudo su ubuntu
+cd /opt/galaxy/tools
+git clone https://github.com/Sage-Bionetworks/SMC-Het-Challenge-Examples.git
+cd SMC-Het-Challenge-Examples
+
+sudo service docker restart
+restart_galaxy
+
+# import data (within Galaxy) using the 'Data' link --> import history
+# test the tools
+# use this file In the 'VCF file' field select 'Tumour1.mutect.vcf'
+# evalate results using the Evaluator Tool in the left hand tool panel
