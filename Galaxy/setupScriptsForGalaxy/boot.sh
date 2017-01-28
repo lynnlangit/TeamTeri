04dc5bbc9790becc0383c6b62d2301002af47ed1
Jim Counts <jcounts18@gmail.com>
2017-01-27 16:56:27 -0800

    tools in boot script

--- Galaxy/setupScriptsForGalaxy/boot.sh
+++ Galaxy/setupScriptsForGalaxy/boot.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-# TODO: figure out a way to parametrize/normalize the disk id.
+#cd /opt/galaxy/tools
+#git clone https://github.com/Sage-Bionetworks/SMC-Het-Challenge-Examples.git
+#service docker restart
+#restart_galaxy
 
-mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-galaxy-data-2
+mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-galaxy-data
 mkdir -p /mnt/disks/data
-mount -o discard,defaults /dev/disk/by-id/google-galaxy-data-2 /mnt/disks/data
+mount -o discard,defaults /dev/disk/by-id/google-galaxy-data /mnt/disks/data
 chmod a+w /mnt/disks/data
-echo UUID=`sudo blkid -s UUID -o value /dev/disk/by-id/google-galaxy-data-2` /mnt/disks/data ext4 discard,defaults,
+echo UUID=`sudo blkid -s UUID -o value /dev/disk/by-id/google-galaxy-data` /mnt/disks/data ext4 discard,defaults,
 nobootwait 0 2 | tee -a /etc/fstab
 cd /mnt/disks/data
 apt-get install gcc python-dev python-setuptools
