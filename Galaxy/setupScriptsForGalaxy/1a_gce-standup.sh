60e339a33e4ebbea2eed13a98b0d7bc2e0cdea85
sptkrishnan <sptkrishnan@gmail.com>
2017-01-28 18:02:47 -0500

    changed SPACES to TAB in 1a

--- Galaxy/setupScriptsForGalaxy/1a_gce-standup.sh
+++ Galaxy/setupScriptsForGalaxy/1a_gce-standup.sh
@@ -17,8 +17,8 @@ gcloud compute images create ${TEAMTERI_IMAGE} \
 
 echo "Creating client instances, please wait..."
 gcloud compute instances create ${GCLOUD_ARGS} ${CLIENT_INSTANCES} \
-    --machine-type ${CLIENT_INSTANCE_TYPE} \
-    --tags ${CLIENT_TAG} \
-    --image ${TEAMTERI_IMAGE} \
-    --image-project ${PROJECT} \
-    --boot-disk-size ${BOOT_DISK_SIZE} 
+	--machine-type ${CLIENT_INSTANCE_TYPE} \
+	--tags ${CLIENT_TAG} \
+	--image ${TEAMTERI_IMAGE} \
+	--image-project ${PROJECT} \
+	--boot-disk-size ${BOOT_DISK_SIZE} 
