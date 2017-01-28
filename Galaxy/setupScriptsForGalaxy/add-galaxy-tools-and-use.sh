67484451cb3485a2f9126f72fd01570dee2009d7
Lynn Langit <lynnlangit@live.com>
2017-01-28 15:28:49 -0800

    added info about testing Galaxy with sample data

--- Galaxy/setupScriptsForGalaxy/add-galaxy-tools-and-use.sh
+++ Galaxy/setupScriptsForGalaxy/add-galaxy-tools-and-use.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ---------------Load example Galaxy Tools------------
-# Using the GCP (GCE) console, click the 'SSH' button to open an SSH shell
+# Use the GCP (GCE) console, click the 'SSH' button to open an SSH shell
 
 sudo su ubuntu
 cd /opt/galaxy/tools
@@ -11,7 +11,10 @@ git clone https://github.com/Sage-Bionetworks/SMC-Het-Challenge-Examples.git
 sudo service docker restart
 restart_galaxy
 
-# import data (within Galaxy) using the 'Data' link --> import history
-# test the tools
-# use this file In the 'VCF file' field select 'Tumour1.mutect.vcf'
-# evalate results using the Evaluator Tool in the left hand tool panel
+# ---------------Test example Galaxy Tools with Sample Data------------
+
+# Import data (within Galaxy) using the 'Data' link --> import history
+# Use this file in the 'VCF file' field select 'Tumour1.mutect.vcf'
+# Click 'Execute' to run the tool - this can take up to 30 minutes to complete
+# Evalate results using the Evaluator Tool (SMC-HeT Evaluator) in the left hand tool panel
+# NOTE: The example is from this link - https://www.synapse.org/#!Synapse:syn2813581/wiki/303161
