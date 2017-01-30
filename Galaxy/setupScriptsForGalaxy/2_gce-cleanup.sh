2a759c2ea9214e3976955795800bdcd52f951a57
Jim Counts <jcounts18@gmail.com>
2017-01-30 10:36:07 -0800

    Update VM scripts

--- Galaxy/setupScriptsForGalaxy/2_gce-cleanup.sh
+++ Galaxy/setupScriptsForGalaxy/2_gce-cleanup.sh
@@ -5,14 +5,13 @@ source ./0_cli.rc
 # ------------------- CLEAN UP  -----------------------
 
 echo "Delete firewall rule"
-gcloud compute firewall-rules delete --project ${PROJECT} galaxy-allow-http
+gcloud compute firewall-rules delete --quiet --project ${PROJECT} galaxy-allow-http &
 
 echo "Deleting VM image..."
-gcloud compute images delete --project ${PROJECT} ${TEAMTERI_IMAGE}
-
-echo "TODO: Detaching disks..."
-
-echo "TODO: Deleting disks..."
+gcloud compute images delete --quiet --project ${PROJECT} ${TEAMTERI_IMAGE} &
 
 echo "Deleting VM instances..."
-gcloud compute instances delete --quiet ${GCLOUD_ARGS} ${CLIENT_INSTANCES}
+gcloud compute instances delete --quiet ${GCLOUD_ARGS} ${VANILLA_INSTANCE} ${TOOLS_INSTANCE} &
+
+echo "Waiting for gcloud to delete resources..."
+wait
